da9f4160fbb4352918de5f14b6acdd2253052c8a
arjunhsb18 <arjunhsb30@gmail.com>
2020-03-04 22:25:13 +0700

    Menambah tampilan view profile, rank, solved dan result dashboard

---
+++ application/views/dashboard/tryout_result_dash.php
@@ -0,0 +1,105 @@
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+	<!-- Content Header (Page header) -->
+	<div class="content-header">
+		<div class="container-fluid">
+			<div class="row mb-2">
+				<div class="col-sm-6">
+					<h1 class="m-0 text-dark">Hasil Try Out</h1>
+				</div><!-- /.col -->
+				
+			</div><!-- /.row -->
+		</div><!-- /.container-fluid -->
+	</div>
+	<!-- /.content-header -->
+
+	<!-- Main content -->
+	<section class="content">
+		<div class="container-fluid">
+			<!-- Small boxes (Stat box) -->
+			<div class="row">
+				<div class="col-lg-6 col-6">
+					<!-- small box -->
+					<div class="small-box bg-info">
+						<div class="inner">
+							<h4>Try Out UTBK</h4>
+
+							<p>Kode: KTO0001</p>
+						</div>
+						<div class="icon">
+							<i class="fa fa-desktop"></i>
+						</div>
+						<a class="btn btn-lg-default small-box-footer" type="button" data-toggle="modal" data-target="#modal-lg">Detail <i class="fas fa-arrow-circle-right"></i></a>
+					</div>
+				</div>
+				<div class="col-lg-6 col-6">
+					<!-- small box -->
+					<div class="small-box bg-success">
+						<div class="inner">
+							<h4>Try Out STAN</h4>
+
+							<p>Kode: KTO0001</p>
+						</div>
+						<div class="icon">
+							<i class="fa fa-book"></i>
+						</div>
+						<a class="btn btn-lg-default small-box-footer" type="button" data-toggle="modal" data-target="#modal-lg">Detail <i class="fas fa-arrow-circle-right"></i></a>
+					</div>
+				</div>
+
+				<div class="modal fade" id="modal-lg">
+					<div class="modal-dialog modal-lg">
+						<div class="modal-content">
+							<div class="modal-header">
+								<h4 class="modal-title">Try Out UTBK</h4>
+								<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+									<span aria-hidden="true">×</span>
+								</button>
+							</div>
+							<div class="modal-body">
+								<div style="margin-right:100px;">
+								<table>
+									<tr>
+										<td>Nama </td>
+										<td>&emsp;:</td>
+										<td></td>
+									</tr>
+									<tr>
+										<td>Kode Try Out </td>
+										<td>&emsp;:</td>
+										<td></td>
+									</tr>
+									<tr>
+										<td>Nilai TKA </td>
+										<td>&emsp;:</td>
+										<td></td>
+									</tr>
+									<tr>
+										<td>Nilai TPS </td>
+										<td>&emsp;:</td>
+										<td></td>
+									</tr>
+									<tr>
+										<td>Nilai Kumulatif</td>
+										<td>&emsp;:</td>
+										<td></td>
+									</tr>
+									
+								</table>
+								</div>
+							</div>
+							<div class="modal-footer float-right">
+								
+							<button type="button" class="btn btn-outline-info">Download as PDF</button>
+							</div>
+						</div>
+						<!-- /.modal-content -->
+					</div>
+					<!-- /.modal-dialog -->
+				</div>
+			</div>
+		</div><!-- /.container-fluid -->
+	</section>
+	<!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
